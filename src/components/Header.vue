ec8e78585775b1760a628d917a03fafda9e86594
diepanhng0711 <diepanhng0711@gmail.com>
2022-11-16 17:12:58 +0700

    Init this vuejs crash repo

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green"></Button>
+    <Button text="Update Task" color="blue"></Button>
+    <Button text="Delete Task" color="red"></Button>
+  </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      default: "Hello World",
+    },
+  },
+  components: {
+    Button
+  }
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
